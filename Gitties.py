949cbc96c1804bac9b369bfc9cc37565c290ae3e
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-01-11 09:42:07 +0100

    joke again

--- Gitties.py
+++ Gitties.py
@@ -1,5 +1,5 @@
 def introduce():
     print("Hello, I'm Gittie!")
 
-def add(a, b):
-    return (int(a)+int(b))
+def joke():
+    print("You: 'Ok Google, tell me a joke.'\nGoogle Assistant: 'One joke, coming up! What is a sea monster's favorite snack? Ships and dip.'")
