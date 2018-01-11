72f6d91271e56d93d2c886f1dd7c085edd2c24f4
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-01-11 10:02:27 +0100

    shout def add

--- Gitties.py
+++ Gitties.py
@@ -6,3 +6,6 @@ def add(a, b):
     return (int(a) + int(b))
 def joke():
     print("You: 'Ok Google, tell me a joke.'\nGoogle Assistant: 'One joke, coming up! What is a sea monster's favorite snack? Ships and dip.'")
+
+def shout():
+    print("Ááááááá...")
